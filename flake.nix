9f49d06f0739d27d7137c39ef6bb4097650ede58
Egor Ignatiev <93564463+Veitangie@users.noreply.github.com>
2026-07-19 16:48:21 +0500

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A concurrent HTTP functional and integration testing tool";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgsFor = system: nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages = forAllSystems (system: {
+        default = (pkgsFor system).callPackage ./pkgs/sinq/default.nix { };
+      });
+    };
+}
